0d557e5a26d852e0ba438892c45d530490f5f8f9
Johannes Kling <johannes.kling92@gmail.com>
2018-07-29 09:54:56 +0200

    load player data after login and redirect to player profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,40 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {Credentials} from '../models/Credentials';
-import {Observable} from 'rxjs';
-import {map, take} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {map, switchMap, take} from 'rxjs/operators';
 import {Router} from '@angular/router';
+import {Player} from '../models/Player';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {STAGE} from '../constants/config';
 
 @Injectable()
 export class AuthService {
 
+  user: Player;
+  userDidSignIn = false;
 
-  constructor(private auth: AngularFireAuth, private router: Router) {
-    this.auth.authState.subscribe(state => {
-      if (!state) {
-        this.router.navigate(['/']);
-      }
-    });
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore, private router: Router) {
+    this.getPlayerDataForAuthState()
+      .subscribe((player: Player) => {
+        if (!player) {
+          this.router.navigate(['/']);
+        } else if (this.userDidSignIn) {
+          this.router.navigate(['/user', 'profile', player.id]);
+        }
+
+        this.user = player;
+      });
   }
 
   isUserLoggedIn(): Observable<boolean> {
-    return this.auth.authState.pipe(take(1), map(state => !!state));
+    return this.getPlayerDataForAuthState().pipe(take(1), map(user => !!user));
   }
 
   signIn(credentials: Credentials): Promise<any> {
     return this.auth.auth.signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(_ => this.userDidSignIn = true)
       .catch(error => Promise.reject(error.message));
   }
 
@@ -33,5 +45,29 @@ export class AuthService {
 
   signOut() {
     this.auth.auth.signOut();
+  }
+
+  private getPlayerDataForAuthState(): Observable<Player> {
+
+    if (this.user) {
+      return of(this.user);
+    }
+
+    return this.auth.authState
+      .pipe(
+        switchMap(authState => {
+
+          if (!authState) {
+            return of(null);
+          }
+
+          return this.db.collection<Player>(STAGE + 'players', ref => ref.where('userId', '==', authState.uid)).valueChanges()
+            .pipe(
+              take(1),
+              map(players => players[0] || null)
+            );
+
+        })
+      );
   }
 }
